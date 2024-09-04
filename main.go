9022ccb50fc42a51a403e40424d39df6a4c6464f
anm-jubaer <anm.jubaer@technonext.com>
2024-09-04 17:48:09 +0600

    added github.com/air-verse/air@latest

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("sdfsfsf")
+	fmt.Println("Server Starting")
+
+	app := fiber.New()
+
+	err := app.Listen(":4000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
