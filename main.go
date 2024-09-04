6c1655da624700a40e09cabc0c43f5f834c975b6
anm-jubaer <anm.jubaer@technonext.com>
2024-09-04 17:56:48 +0600

    added first endpoint

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello from the Server"})
+	})
+
 	err := app.Listen(":4000")
 	if err != nil {
 		log.Fatal(err)
